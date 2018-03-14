10c183a596c3296fdec1b21d3576230f0bf889e1
Clara Park <cycp@berkeley.edu>
2018-03-13 21:41:12 -0700

    part 1

---
+++ db/migrate/20180314041830_create_pokemons.rb
@@ -0,0 +1,13 @@
+class CreatePokemons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :level
+      t.integer :trainer_id
+      t.integer :ndex
+      t.references :trainer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
